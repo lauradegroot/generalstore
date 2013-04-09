4ffeabe2b73b9e85f43f90b774162b7173cbba89
Jen Fong-Adwent <jen@ednapiranha.com>
2013-04-09 16:39:26 -0400

    fixups on json load

--- generalstore/media/js/main.js
+++ generalstore/media/js/main.js
@@ -7,16 +7,28 @@ define(['jquery', 'local_settings', 'base/user', 'utils', 'nunjucks', 'templates
     nunjucks.env = new nunjucks.Environment(new nunjucks.HttpLoader('/templates'));
   }
 
-  var user = new User({ env: settings.ENV });
   var currLevel;
+  var defaults;
 
-  try {
-    currLevel = require('../../config/level1.json');
-  } catch (err) {
-    var error = 'No Level 1 configuration file found! Please create one in the config directory';
+  // We need dimensions for our game background images
+  $.getJSON('config/defaults.json', function (d) {
+    defaults = d;
+  }).fail(function (err) {
+    var error = 'No defaults.json configuration file found! Please create one in the config directory';
     utils.loadTemplate('error.html', { message: error });
     throw new Error(error);
-  }
+  });
+
+  // We always assume at least a first level for gameplay
+  $.getJSON('config/level1.json', function (d) {
+    currLevel = d;
+  }).fail(function (err) {
+    var error = 'No level1.json configuration file found! Please create one in the config directory';
+    utils.loadTemplate('error.html', { message: error });
+    throw new Error(error);
+  });
+
+  var user = new User({ env: settings.ENV });
 
   var body = $('body');
 
